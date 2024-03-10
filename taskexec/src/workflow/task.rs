da820ccd3c5c678364888f9b5285368254d4239d
Romzorus <romzorus591@gmail.com>
2024-03-10 20:17:41 +0100

    Adding Debug trait + basic impl for Task

--- taskexec/src/workflow/task.rs
+++ taskexec/src/workflow/task.rs
@@ -3,15 +3,26 @@
 use crate::modules::Module;
 
 // A Task is the interpreted/parsed version of a RawTask.
-pub struct TaskList {
-    pub list: Vec<Task>,
-}
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct Task {
     // Placeholder
     pub task: String,
     pub module: Module
+}
+
+impl Task {
+    pub fn new() -> Task {
+        Task {
+            task: String::from(""),
+            module: Module::Apt
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct TaskList {
+    pub list: Vec<Task>,
 }
 
 impl TaskList {
