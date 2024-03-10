3917b58dcf1482b51f7c20ef87f88878955e761d
Romzorus <romzorus591@gmail.com>
2024-03-11 00:45:57 +0100

    Add PartialEq trait for assert_eq!() and None variant to Module enum for initialization and error cases

--- taskexec/src/modules/mod.rs
+++ taskexec/src/modules/mod.rs
@@ -1,6 +1,7 @@
 pub mod apt;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Module {
+    None, // Used for new() methods, initializations and errors
     Apt
 }
