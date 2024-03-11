368126cb4d686ac0a2895d5643d863cccf17616d
Romzorus <romzorus591@gmail.com>
2024-03-11 23:16:12 +0100

    Changing TaskResult to TaskListResult

--- taskexec/src/prelude.rs
+++ taskexec/src/prelude.rs
@@ -1,6 +1,6 @@
 pub use crate::workflow::assignment::{Assignment, RunningMode};
 pub use crate::workflow::change::{Change, ChangeList};
 pub use crate::workflow::host::{Host, HostList};
-pub use crate::workflow::result::TaskResult;
+pub use crate::workflow::result::TaskListResult;
 pub use crate::workflow::task::{Task, TaskList};
 pub use crate::modules::Module;
