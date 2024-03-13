e88e79db17855e16dde3c26824a55b71fe48543a
Romzorus <romzorus591@gmail.com>
2024-03-14 00:44:39 +0100

    Going with serde_yaml instead of config (test for Yaml first) and adding lowercase renaming to not have to deal with upper cases in task lists

--- taskexec/src/modules/mod.rs
+++ taskexec/src/modules/mod.rs
@@ -3,6 +3,7 @@ use serde::Deserialize;
 pub mod apt;
 
 #[derive(Debug, Clone, Copy, PartialEq, Deserialize)]
+#[serde(rename_all="lowercase")]
 pub enum Module {
     None, // Used for new() methods, initializations and errors
     Apt
--- taskparser/src/fileformats/yaml.rs
+++ taskparser/src/fileformats/yaml.rs
@@ -1,8 +1,8 @@
-use config::Config;
-use taskexec::workflow::task::TaskList;
+use taskexec::workflow::task::Task;
+use serde_yaml;
 
-pub fn yaml_tasklist_parser(raw_content: Config) -> TaskList {
-    raw_content
-        .try_deserialize::<TaskList>()
-        .expect("Problem parsing the content of the file")
+pub fn yaml_tasklist_parser(file_path: &str) -> Vec<Task> {
+    let tasklistfile = std::fs::File::open(file_path).unwrap();
+    let tasklist: Vec<Task> = serde_yaml::from_reader(tasklistfile).unwrap();
+    return tasklist;
 }
