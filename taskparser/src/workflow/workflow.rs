470cd0ce8c57621981933ee5c90cbaaa5c268a82
Romzorus <romzorus591@gmail.com>
2024-03-15 23:39:55 +0100

    Adding placeholder for interactive mode function

--- taskparser/src/workflow/workflow.rs
+++ taskparser/src/workflow/workflow.rs
@@ -18,3 +18,10 @@ pub fn tasklist_parser(tasklistcontent: String, format: ContentFormat) -> TaskLi
 pub fn tasklist_get_from_file(file_path: &str) -> String {
     std::fs::read_to_string(file_path).unwrap() // Placeholder : error handling required here
 }
+
+pub fn tasklist_get_from_interactive_mode() -> String {
+    // Placeholder : interactive_mode is when the final user sets a group of hosts and can
+    // type commands as in a normal shell interpreter but each command is directly turned
+    // into a Task, executed on the group of hosts, and the results arrive in "realtime".
+    String::new()
+}
