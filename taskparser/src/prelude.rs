06518862a290ebd33955b082e13da42ba6d2d3fe
Romzorus <romzorus591@gmail.com>
2024-03-15 23:00:19 +0100

    Adding tasklist_get_from_file

--- taskparser/src/prelude.rs
+++ taskparser/src/prelude.rs
@@ -1,2 +1,3 @@
 pub use crate::fileformats::{json, toml, yaml};
-pub use crate::workflow::workflow;
+pub use crate::workflow::workflow::{tasklist_parser, tasklist_get_from_file};
+pub use config::FileFormat;
