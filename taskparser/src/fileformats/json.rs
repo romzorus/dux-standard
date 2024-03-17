62d26d075e90e9d1ca6e0cb81882e9f30a91704e
Romzorus <romzorus591@gmail.com>
2024-03-17 14:02:09 +0100

    Adding JSON format to TaskList parsing capability

--- taskparser/src/fileformats/json.rs
+++ taskparser/src/fileformats/json.rs
@@ -1,8 +1,7 @@
-use taskexec::workflow::task::TaskList;
+use taskexec::workflow::task::{Task, TaskList};
+use serde_json;
 
 pub fn json_tasklist_parser(tasklistcontent: &String) -> TaskList {
-    // tasklistcontent
-    //     .try_deserialize::<TaskList>()
-    //     .expect("Problem parsing the content of the file")
-    TaskList::new()
+    let tasklist: Vec<Task> = serde_json::from_str(tasklistcontent).unwrap();
+    return TaskList::from(tasklist)
 }
