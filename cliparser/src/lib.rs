748b9b9610d66bc0cb4aab4f619707f57bf94228
Romzorus <romzorus591@gmail.com>
2024-03-21 23:35:26 +0100

    CliArgs and parse_cli_args() creation

--- cliparser/src/lib.rs
+++ cliparser/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use clap::Parser;
+
+/// Run Dux as a all-in-one tool
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct CliArgs {
+    /// Path to configuration file
+    #[arg(short, long)]
+    pub conf: Option<String>,
+
+    /// Path to TaskList file
+    #[arg(short, long)]
+    pub tasklist: Option<String>,
+
+    /// Path to HostList file
+    #[arg(short = 'l', long)]
+    pub hostlist: Option<String>,
+
+    /// Path to SSH keys folder
+    #[arg(short, long)]
+    pub sshkeys: Option<String>,
+
+    /// Username to use on remote hosts
+    #[arg(short, long)]
+    pub user: Option<String>,
+
+    /// Password to use on remote hosts
+    #[arg(short, long)]
+    pub password: Option<String>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn parse_cli_args() -> CliArgs {
+    CliArgs::parse()
 }
