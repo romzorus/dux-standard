e94d902d9338e1c77ac93446649f842ab8af014d
Romzorus <romzorus591@gmail.com>
2024-03-24 23:55:42 +0100

    Adding necessary errors for HostParser + explanations

--- taskexec/src/workflow/error.rs
+++ taskexec/src/workflow/error.rs
@@ -1,4 +1,8 @@
 #[derive(Debug)]
 pub enum Error {
-    MissingInitialization
+    FailureToFindGroupContent,  // Used in HostParser lib by hostlist_get_specific_group()
+    MissingInitialization,      // Used for CorrelationId
+    GroupNotFound,              // Used in HostParser lib by hostlist_get_specific_group()
+    MissingGroupsList,          // Used in HostParser lib by hostlist_get_specific_group()
+    
 }
