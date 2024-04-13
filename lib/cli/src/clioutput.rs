a3739b96ff3d460d9d5fedd2d9149f5fffe4c2c1
Romzorus <romzorus591@gmail.com>
2024-04-14 01:17:48 +0200

    Since there is the new AssignmentFinalStatus field, the output is now different depending on this field.

--- lib/cli/src/clioutput.rs
+++ lib/cli/src/clioutput.rs
@@ -1,6 +1,6 @@
 use termimad::crossterm::style::Color::*;
 use termimad::*;
-use taskexec::{prelude::*, workflow::assignment};
+use taskexec::prelude::*;
 
 pub fn display_output(assignment: Assignment) {
 
@@ -10,67 +10,79 @@ pub fn display_output(assignment: Assignment) {
 
     let mut table_content = String::new();
 
-    table_content.push_str(format!("Host : ***{}***", assignment.host).as_str());
+    table_content.push_str(format!("Host : ***{}*** ({})", assignment.host, output_nice_finalstatus(&assignment.finalstatus)).as_str());
 
-    table_content.push_str("\n|-:|:-:|:-:|-");
-    table_content.push_str("\n|**Task**|**Step**|**Changes**|**Results**|");
-   
-    for (taskblockindex, taskblock) in assignment.tasklist.tasks.iter().enumerate() {
-        // A "step" is a ModuleBlockExpectedState (simplifies the reading)
-        for (stepindex, step) in taskblock.steps.iter().enumerate() {
-            table_content.push_str("\n|-:|:-|:-:|-");
-            
-            match &assignment.changelist.taskchanges.clone().unwrap()[taskblockindex].stepchanges[stepindex] {
-                ModuleBlockChange::AlreadyMatched(message) => {
-                    table_content.push_str(
-                        format!("\n|{}|{}| Matched : {}|{}|",
-                                taskblock.name.clone().unwrap_or(String::from("no name for TaskBlock")),
-                                output_nice_step(&step),
-                                message,
-                                "N/A"
-                            ).as_str()
-                    );
-                }
-                ModuleBlockChange::FailedToEvaluate(message) => {
-                    table_content.push_str(
-                        format!("\n|{}|{}| Failed to evaluate : {}|{}|",
-                                taskblock.name.clone().unwrap_or(String::from("no name for TaskBlock")),
-                                output_nice_step(&step),
-                                message,
-                                "N/A"
-                            ).as_str()
-                    );
-                }
-                ModuleBlockChange::ModuleApiCalls(apicalls) => {
-                    table_content.push_str(
-                        format!("\n|{}|{}|{}|{}|",
-                            taskblock.name.clone().unwrap_or(String::from("no name for TaskBlock")),
-                            output_nice_step(&step),
-                            assignment.changelist.taskchanges.clone().unwrap()[taskblockindex].stepchanges.clone()[stepindex].display()[0],
-                            output_nice_result(&assignment.tasklistresult.clone().taskresults[taskblockindex].stepresults.clone().unwrap()[stepindex].apicallresults[0].status)
-                        ).as_str()
-                    );
+    match assignment.finalstatus {
+        // AssignmentFinalStatus::Unset => {}
+        AssignmentFinalStatus::Failed(error) => {
+            table_content.push_str("\n|-:|:-:|");
+            table_content.push_str(
+                format!("\n|**Failed**|{}|", error).as_str()
+            );
+        }
+        _ => {
+            table_content.push_str("\n|-:|:-:|:-:|-");
+            table_content.push_str("\n|**Task**|**Step**|**Changes**|**Results**|");
+           
+            for (taskblockindex, taskblock) in assignment.tasklist.tasks.iter().enumerate() {
+                // A "step" is a ModuleBlockExpectedState (simplifies the reading)
+                for (stepindex, step) in taskblock.steps.iter().enumerate() {
+                    table_content.push_str("\n|-:|:-|:-:|-");
                     
-                    for (apicallindex, apicallcontent) in apicalls.iter().enumerate() {
-                        if apicallindex > 0 {
+                    match &assignment.changelist.taskchanges.clone().unwrap()[taskblockindex].stepchanges[stepindex] {
+                        ModuleBlockChange::AlreadyMatched(message) => {
                             table_content.push_str(
-                                format!("\n|||{}|{}|",
-                                    assignment.changelist.taskchanges.clone().unwrap()[taskblockindex].stepchanges.clone()[stepindex].display()[apicallindex],
-                                    output_nice_result(&assignment.tasklistresult.clone().taskresults[taskblockindex].stepresults.clone().unwrap()[stepindex].apicallresults[apicallindex].status)
+                                format!("\n|{}|{}| Matched : {}|{}|",
+                                        taskblock.name.clone().unwrap_or(String::from("no name for TaskBlock")),
+                                        output_nice_step(&step),
+                                        message,
+                                        "N/A"
+                                    ).as_str()
+                            );
+                        }
+                        ModuleBlockChange::FailedToEvaluate(message) => {
+                            table_content.push_str(
+                                format!("\n|{}|{}| Failed to evaluate : {}|{}|",
+                                        taskblock.name.clone().unwrap_or(String::from("no name for TaskBlock")),
+                                        output_nice_step(&step),
+                                        message,
+                                        "N/A"
+                                    ).as_str()
+                            );
+                        }
+                        ModuleBlockChange::ModuleApiCalls(apicalls) => {
+                            table_content.push_str(
+                                format!("\n|{}|{}|{}|{}|",
+                                    taskblock.name.clone().unwrap_or(String::from("no name for TaskBlock")),
+                                    output_nice_step(&step),
+                                    assignment.changelist.taskchanges.clone().unwrap()[taskblockindex].stepchanges.clone()[stepindex].display()[0],
+                                    output_nice_result(&assignment.tasklistresult.clone().taskresults[taskblockindex].stepresults.clone().unwrap()[stepindex].apicallresults[0].status)
                                 ).as_str()
                             );
+                            
+                            for (apicallindex, apicallcontent) in apicalls.iter().enumerate() {
+                                if apicallindex > 0 {
+                                    table_content.push_str(
+                                        format!("\n|||{}|{}|",
+                                            assignment.changelist.taskchanges.clone().unwrap()[taskblockindex].stepchanges.clone()[stepindex].display()[apicallindex],
+                                            output_nice_result(&assignment.tasklistresult.clone().taskresults[taskblockindex].stepresults.clone().unwrap()[stepindex].apicallresults[apicallindex].status)
+                                        ).as_str()
+                                    );
+                                }
+                            }
                         }
                     }
                 }
             }
         }
     }
+
+    
     
     /////////////////////////////////////////////////////////////////////////////
 
     table_content.push_str("\n|-");
     println!("{}", skin.term_text(&table_content));
-    println!("\n");
 }
 
 //pub fn display_results_detailed() {}
@@ -89,4 +101,21 @@ fn output_nice_result(status: &ApiCallStatus) -> String {
 // TODO : improve this / replace with step name when it will be implemented
 fn output_nice_step(step: &ModuleBlockExpectedState) -> String {
     return format!("`{:?}`", step);
+}
+
+fn output_nice_finalstatus(finalstatus: &AssignmentFinalStatus) -> String {
+    match finalstatus {
+        AssignmentFinalStatus::Unset => {
+            return String::from("Unset");
+        }
+        AssignmentFinalStatus::Failed(_error) => {
+            return format!("Failed");
+        }
+        AssignmentFinalStatus::Changed => {
+            return String::from("Changed");
+        }
+        AssignmentFinalStatus::AlreadyMatched => {
+            return String::from("Matched");
+        }
+    }
 }
