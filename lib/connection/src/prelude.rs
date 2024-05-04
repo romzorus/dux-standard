1feb6117967a39bddea920ef245c97d8c583a409
Romzorus <romzorus591@gmail.com>
2024-05-04 23:15:35 +0200

    Introducing REFRESH_INTERVAL_MILLI_SECONDS to be used by consumer/producer loops

--- lib/connection/src/prelude.rs
+++ lib/connection/src/prelude.rs
@@ -1,3 +1,4 @@
 pub use crate::{HostHandler, ConnectionMode, CmdResult, Privilege};
 pub use crate::ssh2mode::{Ssh2AuthMode, Credentials};
+pub use crate::REFRESH_INTERVAL_MILLI_SECONDS;
 // TODO : add SSH3 support (https://github.com/francoismichel/ssh3)
