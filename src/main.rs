ab0d38b279eb2132286df006d68833880bc31a6b
Romzorus <romzorus591@gmail.com>
2024-11-07 22:08:46 +0100

    Switch to duxcore new version

--- src/main.rs
+++ src/main.rs
@@ -20,158 +20,91 @@ fn main() {
         DuxConfigStandard::from(cliargs.conf).expect("Unable to determine configuration. Abort.");
 
     // Build a HostList
-    let hostlist = match &cliargs.hostlist {
-        Some(hostlist_path) => {
-            match HostList::from_file(&hostlist_path) {
-                Ok(hostlist) => {
-                    hostlist
-                }
-                Err(error) => {
-                    println!("Unable to read hoslist file. Abort.");
-                    println!("{:?}", error);
-                    exit(1);
-                }
+    let host_list = match &cliargs.hostlist {
+        Some(hostlist_path) => match HostList::from_file(&hostlist_path) {
+            Ok(hostlist) => hostlist,
+            Err(error) => {
+                println!("Unable to read hoslist file. Abort.");
+                println!("{:?}", error);
+                exit(1);
             }
-        }
+        },
         None => {
             println!("No hostlist path provided. Abort.");
             exit(1);
         }
     };
-    
 
-    if hostlist_get_all_hosts(&hostlist).is_none() {
+    // If hostlist is empty, stop there
+    if hostlist_get_all_hosts(&host_list).is_none() {
         println!("No hosts in given list ({})", cliargs.hostlist.unwrap());
         exit(0);
     }
 
-    // Build Assignments (an Assignment is basically a Host associated to a TaskList)
-    //  -> Initialization of CorrelationId (not really required for all-in-one mode)
-    let mut correlationid = CorrelationIdGenerator::new();
-    match correlationid.init() {
-        Ok(_) => {}
-        Err(e) => {
-            println!("Error: failure to initialize CorrelationId");
-            println!("{:?}", e);
-            exit(1);
+    // How do we connect to all hosts ?
+    let host_connection_info = match &cliargs.key {
+        Some(privatekeypath) => {
+            HostConnectionInfo::ssh2_with_key_file(&cliargs.user.unwrap(), privatekeypath)
         }
-    }
-    //  -> Actual build of Assignments
-    let mut assignmentlist: Vec<Assignment> = Vec::new();
-
-    // This unwrap is safe since we checked before that the list is not empty.
-    for host in hostlist.hosts.as_ref().unwrap() {
-        let authmode = match &cliargs.key {
-            Some(privatekeypath) => Ssh2AuthMode::KeyFile((
-                cliargs.user.clone().unwrap(),
-                PathBuf::from(privatekeypath),
-            )),
-            None => {
-                // No SSH key given as argument, trying with password if it is given
-                match cliargs.password.clone() {
-                    Some(pwd) => Ssh2AuthMode::UsernamePassword(Credentials::from(
-                        cliargs.user.clone().unwrap(),
-                        pwd,
-                    )),
-                    None => {
-                        panic!("No SSH key or password to connect to remote host.");
-                        // TODO : gracefully quit instead of panic
-                    }
+        None => {
+            // No SSH key given as argument, trying with password if it is given
+            match cliargs.password.clone() {
+                Some(pwd) => {
+                    HostConnectionInfo::ssh2_with_username_password(cliargs.user.unwrap(), pwd)
+                }
+                None => {
+                    panic!("No SSH key or password to connect to remote host.");
+                    // TODO : gracefully quit instead of panic
                 }
             }
-        };
-
-        // let tasklist = tasklist_parser(
-        //     tasklist_get_from_file(&cliargs.tasklist.as_ref().unwrap()),
-        //     &host
-        //     );
-        
-        let tasklist = match &cliargs.tasklist {
-            Some(tasklist_path) => {
-                match TaskList::from_file(tasklist_path, TaskListFileType::Unknown, &host) {
-                    Ok(tasklist) => {
-                        tasklist
-                    }
-                    Err(error) => {
-                        println!("Unable to build tasklist. Abort.");
-                        println!("{:?}", error);
-                        exit(1);
-                    }
-                }
-            }
-            None => {
-                println!("No hostlist path provided. Abort.");
-                exit(1);
-            }
-        };
-    
-        if tasklist.tasks.is_empty() {
-            println!("No task in given list ({})", &cliargs.tasklist.unwrap());
-            exit(0);
         }
-
-        // SSH2 is assumed for now.
-        assignmentlist.push(Assignment::from(
-            correlationid.get_new_value().unwrap(), // This unwrap() is safe because initialization is checked before.
-            RunningMode::Apply,
-            host.address.clone(),
-            HostHandlingInfo::from(
-                ConnectionMode::Ssh2,
-                host.address.clone(),
-                ConnectionDetails::Ssh2(Ssh2ConnectionDetails::from(
-                    host.address.clone(),
-                    authmode,
-                )),
-            ),
-            HashMap::new(),
-            tasklist.clone(),
-            ChangeList::new(),
-            ResultList::new(),
-            AssignmentFinalStatus::Unset,
-        ));
-    }
-
-    // If the number of threads to use is not specified, one thread per CPU of the local machine
-    let threads_number = match cliargs.threads {
-        None => std::thread::available_parallelism().unwrap().get(),
-        Some(number) => number,
     };
 
-    let resultslist: Mutex<Vec<Assignment>> = Mutex::new(Vec::new());
+    // Build a JobList based on all previous work (HostList, TaskList, CLI arguments and so on...)
+    let mut job_list = JobList::from_hostlist(host_list);
 
-    let pool = rayon::ThreadPoolBuilder::new()
-        .num_threads(threads_number)
-        .build()
+    job_list
+        .set_connection(host_connection_info)
+        .unwrap()
+        .set_tasklist_from_file(&cliargs.tasklist.unwrap(), TaskListFileType::Unknown)
         .unwrap();
 
-    pool.install(|| {
-        rayon::scope(|s| {
-            for mut assignment in assignmentlist.into_iter() {
-                let resultslist = &resultslist;
-                s.spawn(move |_| {
-                    let mut hosthandler = HostHandler::from(&assignment.hosthandlinginfo).unwrap();
 
-                    let _ = hosthandler.init();
+    job_list.apply();
 
-                    let _ = assignment.dry_run(&mut hosthandler);
-                    if let AssignmentFinalStatus::Unset = assignment.finalstatus {
-                        let _ = assignment.apply(&mut hosthandler);
-                    }
-                    resultslist.lock().unwrap().push(assignment);
-                });
-            }
-        });
-    });
+    println!("{}", job_list.display_pretty());
+    // // If the number of threads to use is not specified, one thread per CPU of the local machine
+    // let threads_number = match cliargs.threads {
+    //     None => std::thread::available_parallelism().unwrap().get(),
+    //     Some(number) => number,
+    // };
 
-    // TODO : implement a better way to sort the output according to the order of the hosts in the HostList
-    // aka sort resultslist in HostList order so we simply have to go through resultslist after that
-    for host in hostlist.hosts.unwrap() {
-        for assignment in resultslist.lock().unwrap().clone().into_iter() {
-            if host.address.eq(&assignment.host) {
-                display_output(assignment);
-            }
-        }
-    }
+    // let resultslist: Mutex<Vec<Assignment>> = Mutex::new(Vec::new());
+
+    // let pool = rayon::ThreadPoolBuilder::new()
+    //     .num_threads(threads_number)
+    //     .build()
+    //     .unwrap();
+
+    // pool.install(|| {
+    //     rayon::scope(|s| {
+    //         for mut assignment in assignmentlist.into_iter() {
+    //             let resultslist = &resultslist;
+    //             s.spawn(move |_| {
+    //                 let mut hosthandler = HostHandler::from(&assignment.hosthandlinginfo).unwrap();
+
+    //                 let _ = hosthandler.init();
+
+    //                 let _ = assignment.dry_run(&mut hosthandler);
+    //                 if let AssignmentFinalStatus::Unset = assignment.finalstatus {
+    //                     let _ = assignment.apply(&mut hosthandler);
+    //                 }
+    //                 resultslist.lock().unwrap().push(assignment);
+    //             });
+    //         }
+    //     });
+    // });
+
 }
 
 pub fn welcome_message_standard() {
